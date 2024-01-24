2d63b2d87b8f9144ba0f283ea3185319c8bf981e
azizbek400 <aziznormurodov400@gmail.com>
2024-01-25 01:01:13 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,144 @@
+// 1-masala
+
+// let a = 5
+// let b = 4
+// let c = 4 * 5
+// console.log(c);
+
+// 2-masala
+
+// let a = 7
+// let c = 7 * 7
+// console.log(c);
+
+// 3-masala
+
+// let a = 2
+// let b = 9
+// let c = 3
+// let L = 2 * 9 * 3
+// console.log(L);
+
+// let d = 3 * 3
+// let S = d * b
+// console.log(S);
+
+// 4-masala
+
+// let a = 16
+// let b = 4
+// let c = 8
+// let d = a * b
+// let G = d / c
+// console.log(G);
+
+// 5-masala 
+
+// let a = 3
+// let b = 6
+// let c = 7
+// let d = 8
+// let e = d * d
+// let f = d * d * d * d * d * d 
+// let y = a * f - b * e - c
+// console.log(y);
+
+// 6-masala
+
+// let x = 36000
+// let a = 6
+// let b = x / a
+// console.log(b);
+
+// 7-masala
+
+// let a = 80
+// let b = 58
+// let c
+// c = a
+// a = b
+// b = c
+// console.log(a);
+// console.log(b);
+
+// 8-masala
+
+// let a = 10
+// let b = 6
+// let C = a * b 
+// let P = 2 * (a + b)
+// console.log(C);
+// console.log(P);
+
+// 9-masala
+
+// let a = 3.14
+// let d = 20
+// let b = a * d
+// console.log(b);
+
+// 10-masala
+
+// let a = 5
+// let b = 6
+// let V = a **3
+// let C = b * a **2
+// console.log(V);
+// console.log(C);
+
+// 11-masala
+
+// let a = 5
+// let b = 8
+// let c = 6
+// let V = a * b * c
+// let S = 2*(a*b+b*c+a*c)
+// console.log(V);
+// console.log(S);
+
+// 12-masala
+
+// let a = 12
+// let b = 24
+// let M = (a+b)/2
+// console.log(M);
+
+// 13-masala
+
+// let a = 8;
+// let b = 6;
+// let q = 10;
+// let y = a**2 + b**2;
+// let S = y / q;
+
+// let P = a + b + S;
+
+// console.log(S, P);
+
+// 14-masala
+
+// let a = 23
+// let b = a **3;
+// let c = a **6;
+// let d = a **9;
+// let e = a **15;
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+// 15-masala
+
+// let x = 5
+// let a = 25000
+// let y = 6
+// let b = 30000
+// let H = a + b
+// console.log(H);
+
+
+
+
+
+
+
